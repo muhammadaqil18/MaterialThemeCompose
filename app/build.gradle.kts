20b30d13e306830ecaa2e53e7cb81ffa3606c00b
muhammadaqil18 <aqil.pratama@koinworks.com>
2023-10-29 15:52:15 +0700

    add scaffolding in list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.05.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
